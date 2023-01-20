7395a123bf619556b543756d913499a32465bb49
Silvio Maia <ingles.silvio@gmail.com>
2023-01-20 19:41:34 -0300

    first commit

---
+++ src/main/java/br/com/persondoc/database/persondoc/PersonDocApplication.java
@@ -0,0 +1,13 @@
+package br.com.persondoc.database.persondoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonDocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonDocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/persondoc/database/persondoc/PersonDocApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.persondoc.database.persondoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonDocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
